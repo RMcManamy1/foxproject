f3b561e34b292e851d92c24bd81398bd5c83e9a3
RMcManamy1 <39100792+RMcManamy1@users.noreply.github.com>
2018-05-31 08:52:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+var $menu_opener = document.getElementByID( 'nav-jump' ),
+    $menu_closer = document.getElementByID( 'menu-close' );
 
+var toggleDrawerNav_running = false;
+function toggleDrawerNav( event ) {
+   event.preventDefault();
+  
+  // check to make sure it's not already running
+  if ( toggleDrawerNav_running ) {return; }
+ 
+  // block any secondary request until we're done
+  toggleDrawerNav_running = true;
+  
+  page_classes.toggle( 'drawer-nav-open' );
+}
+
+var toggleDrawerNav_running = false;
+function toggleDrawerNav( event ) {
+
+// the code yo usaw before
+  
+//toggle the "running indicator off once the animation is complete. Duration: .5 seconds
+  setTimeout(function(){
+    toggleDrawerNav_running = false;
+  }, 500);
+}
+
+$menu_opener
+  .addEventListener( 'click', toggleDrawerNav, false );
+$menu_opener
+  .addEventListener( 'touchdown', toggleDrawerNav, false );
+$menu_closer
+  .addEventListener( 'click', toggleDrawerNav, false );
+$menu_closer
+  .addEventListener( 'touchdown', toggleDrawerNav, false );
+
+function toggleDrawerNav( event ) {
+   event.preventDefault();
+  
+//Toggle the "open" class on the html element page_classes.toggle( 'drawer-nav-open' );
+}
